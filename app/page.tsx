4e5ab80e1a304ef1e77a3537ac80bf4d5b2d7714
German <g.gabrielpuleo@gmail.com>
2025-02-06 15:47:03 +0100

    styling and navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import Navbar from "./components/Navbar";
 import TierList from "./components/TierList";
 
 export default function Home() {
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold">Tier List Generator</h1>
-      <TierList />
+    <div>
+      <Navbar />
+      <div className="p-8">
+        <TierList />
+      </div>
     </div>
   );
 }
